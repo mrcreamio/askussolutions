2cd6b7527c7677028e6330d780b8c7dbba16e01c
Ahmed Waheed <mrcreamio@outlook.com>
2022-02-20 21:24:44 +0500

    refactored

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,76 +1,60 @@
-import React, { useState }  from "react"
-import axios from "axios";
+import * as React from "react"
 import { Link } from "gatsby"
-import Layout from "../components/layout"
-const MyForm = () => {
-    
-    const [serverState, setServerState] = useState({
-      submitting: false,
-      status: null
-    });
-    const handleServerResponse = (ok, msg, form) => {
-      setServerState({
-        submitting: false,
-        status: { ok, msg }
-      });
-      if (ok) {
-        form.reset();
-      }
-    };
-    const handleOnSubmit = e => {
-      e.preventDefault();
-      const form = e.target;
-      setServerState({ submitting: true });
-      axios({
-        method: "post",
-        url: "https://getform.io/f/4113f349-e99f-495a-b529-18c2cb40e11c",
-        data: new FormData(form)
-      })
-        .then(r => {
-          handleServerResponse(true, "Thanks!", form);
-        })
-        .catch(r => {
-          handleServerResponse(false, r.response.data.error, form);
-        });
-    };
-    return (
-        <Layout>
-    
-    <div>
-         <div className="col-md-8 mt-5">
-            <h3>Getform.io Gatsby Form Example</h3>
-            <form onSubmit={handleOnSubmit}>
-            <div className="form-group">
-                <label for="exampleInputEmail1" required="required">Email address</label>
-                <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-            </div> 
-            <div className="form-group">
-                <label for="exampleInputName">Name</label>
-                <input type="text" name="name" className="form-control" id="exampleInputName" placeholder="Enter your name" required="required"/>
-            </div>
-            <div className="form-group">
-                <label for="exampleFormControlSelect1">Favourite Platform</label>
-                <select className="form-control" id="exampleFormControlSelect1" name="platform" required="required">
-                <option>Github</option>
-                <option>Gitlab</option>
-                <option>Bitbucket</option>
-                </select>
-            </div>
-            <button type="submit" className="btn btn-primary"  disabled={serverState.submitting}>
-                Submit
-            </button>
-            {serverState.status && (
-                <p className={!serverState.status.ok ? "errorMsg" : ""}>
-                {serverState.status.msg}
-                </p>
-            )}
-            </form>
-        </div>
-      </div>  
-    
+import { Parallax } from "@react-spring/parallax"
+import { Themed } from "theme-ui"
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
+import Divider from "@lekoarts/gatsby-theme-cara/src/elements/divider"
+import { UpDown, UpDownWide } from "@lekoarts/gatsby-theme-cara/src/styles/animations"
+import Svg from "@lekoarts/gatsby-theme-cara/src/components/svg"
+import Seo from "@lekoarts/gatsby-theme-cara/src/components/seo"
+import Content from "@lekoarts/gatsby-theme-cara/src/elements/content"
+import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
+
+const NotFound = () => (
+  <Layout>
+    <Seo title="404 - Not Found" />
+    <Parallax pages={1}>
+      <div>
+        <Divider speed={0.2} offset={0} factor={1}>
+          <UpDown>
+            <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
+            <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
+            <Svg icon="box" width={6} color="icon_darker" left="60%" top="15%" />
+          </UpDown>
+          <UpDownWide>
+            <Svg icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
+            <Svg icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
+            <Svg icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
+            <Svg icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
+            <Svg icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
+            <Svg icon="circle" width={6} color="icon_darkest" left="75%" top="10%" />
+            <Svg icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
+          </UpDownWide>
+          <Svg icon="circle" hiddenMobile width={24} color="icon_darker" left="5%" top="70%" />
+          <Svg icon="circle" width={6} color="icon_darkest" left="4%" top="20%" />
+          <Svg icon="circle" width={12} color="icon_darkest" left="50%" top="60%" />
+          <Svg icon="upDown" width={8} color="icon_darkest" left="95%" top="90%" />
+          <Svg icon="upDown" hiddenMobile width={24} color="icon_darker" left="40%" top="80%" />
+          <Svg icon="triangle" width={8} stroke color="icon_darker" left="25%" top="5%" />
+          <Svg icon="circle" width={64} color="icon_green" left="95%" top="5%" />
+          <Svg icon="box" hiddenMobile width={64} color="icon_purple" left="5%" top="90%" />
+          <Svg icon="box" width={6} color="icon_darkest" left="10%" top="10%" />
+          <Svg icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
+          <Svg icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
+          <Svg icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
+        </Divider>
+        <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0} factor={1}>
+          <Inner>
+            <Themed.h1>contact Us</Themed.h1>
+            <textarea name="input" id="input" cols="30" rows="10">Your Name</textarea>
+            <Themed.p>
+              Go back to <Link to="/">homepage</Link>.
+            </Themed.p>
+          </Inner>
+        </Content>
+      </div>
+    </Parallax>
   </Layout>
-     
-    );
-  };
-  
-  export default MyForm;
+)
+
+export default NotFound
